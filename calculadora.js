7f270c9e8fd9827989229232a6b4017d34b4eb95
fernando-iwerson <fernandoiwerson@gmail.com>
2023-07-29 21:45:19 -0300

    first commit

---
+++ calculadora.js
@@ -0,0 +1,25 @@
+function calculadora(n1,n2,operador){
+    
+
+    if(operador == '+'){
+        console.log(n1+n2);
+    }
+    else if(operador == '-'){
+        console.log(n1-n2);
+    }
+    else if(operador == '*'){
+        console.log(n1*n2);
+    }
+    else if(operador == '/'){
+        console.log(n1/n2);
+    }
+    else {
+        console.log(0)
+    }
+}
+calculadora(7,3,'*');
+
+
+
+
+
